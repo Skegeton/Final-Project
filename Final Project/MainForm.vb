723645fc1842f311055d141df437814961c76df4
SKEGETON-PC\joshu_000 <joshuaheal007@gmail.com>
2016-11-14 23:45:35 -0500

    Added prompts to go to both the menu and exiting the program

--- Final Project/MainForm.vb
+++ Final Project/MainForm.vb
@@ -1,15 +1,17 @@
 ﻿Public Class MainForm
     Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim input As String = Nothing
-        input = InputBox("How many are in you party?", "Waiter Simulator", , , )
+        input = InputBox("How many are in you party?", "Party Quantity", , , )
     End Sub
 
 
     Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        MessageBox.Show("Thank you for coming to Pateli's!")
         Close()
     End Sub
 
     Private Sub MenuToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles MenuToolStripMenuItem.Click
+        MessageBox.Show("What can I get you started with?")
         MenuForm.ShowDialog()
     End Sub
 End Class
